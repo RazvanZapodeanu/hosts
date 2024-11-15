8dfe924212c8b3998f1fb778bbc9c6471baf50c4
Razvan Zapodeanu <razvan.zapodeanu@s.unibuc.ro>
2024-11-15 13:02:43 +0000

    Modified to use DNS function for IP and host verification

--- verifyIP.sh
+++ verifyIP.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-verify_ip() {
-    ip="$1"
-    if ping -c 1 "$ip" &> /dev/null
-    then
-        echo "IP $ip is valid."
+verify_dns_association() {
+    host_name=$1
+    ip_address=$2
+    dns_server=$3
+
+    resolved_ip=$(dig +short "$host_name" @"$dns_server")
+    if [ "$resolved_ip" == "$ip_address" ]; then
+        echo "The IP $ip_address is correctly associated with $host_name."
     else
-        echo "IP $ip is not valid."
+        echo "The IP $ip_address is not correctly associated with $host_name."
     fi
 }
 
 while read line; do
+    host=$(echo $line | awk '{print $2}')
     ip=$(echo $line | awk '{print $1}')
+
     if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        verify_ip $ip
+        verify_dns_association $host $ip "8.8.8.8"
     fi
 done < /etc/hosts
+
